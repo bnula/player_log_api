72a29dd115d1782073baa09bcfb89dcfa8337ae2
bnula <bnulagnula@gmail.com>
2021-02-15 22:15:32 +0100

    added todo list, added NpcsController and CRUD funcions

--- player_log_api/Controllers/NpcsController.cs
+++ player_log_api/Controllers/NpcsController.cs
@@ -12,40 +12,40 @@ using System.Threading.Tasks;
 namespace player_log_api.Controllers
 {
     /// <summary>
-    /// Endpoint for interacting with Location Table in PLayer Log API
+    /// Endpoint for interacting with Npc table of Player Log API
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class LocationsController : ControllerBase
+    public class NpcsController : ControllerBase
     {
-        private readonly ILocationRepository _locRepo;
+        private readonly INpcRepository _npcRepo;
         private readonly ILoggerService _logger;
         private readonly IMapper _mapper;
-        public LocationsController(
-            ILocationRepository locRepo,
+        public NpcsController(
+            INpcRepository npcRepo,
             ILoggerService logger,
             IMapper mapper)
         {
-            _locRepo = locRepo;
+            _npcRepo = npcRepo;
             _logger = logger;
             _mapper = mapper;
         }
 
         /// <summary>
-        /// Get all Location records
+        /// Get all Npc records
         /// </summary>
-        /// <returns>List of Location records</returns>
+        /// <returns>List of Npc records</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetLocations()
+        public async Task<IActionResult> GetNpcs()
         {
             var controllerName = GetControllerActionNames();
             try
             {
-                _logger.LogInfo($"{controllerName}: Attemted Call");
-                var items = await _locRepo.FindAll();
-                var response = _mapper.Map<List<LocationDTO>>(items);
+                _logger.LogInfo($"{controllerName}: Attempted Call");
+                var items = await _npcRepo.FindAll();
+                var response = _mapper.Map<List<NpcDTO>>(items);
                 _logger.LogInfo($"{controllerName}: Success");
                 return Ok(response);
             }
@@ -54,18 +54,18 @@ namespace player_log_api.Controllers
                 return InternalError($"{controllerName}: {ex.Message} - {ex.InnerException}");
             }
         }
-
+        
         /// <summary>
-        /// Get a single Location record
+        /// Get an Npc record
         /// </summary>
         /// <param name="id"></param>
-        /// <returns>a Location record</returns>
+        /// <returns>Npc record</returns>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetLocation(int id)
+        public async Task<IActionResult> GetNpc(int id)
         {
             var controllerName = GetControllerActionNames();
             try
@@ -76,15 +76,14 @@ namespace player_log_api.Controllers
                     _logger.LogWarn($"{controllerName}: Invalid ID - Item ID: {id}");
                     return BadRequest();
                 }
-                var item = await _locRepo.FindByID(id);
-                if (item == null)
+                if (!await _npcRepo.RecordExistsByID(id))
                 {
                     _logger.LogWarn($"{controllerName}: Not Found - Item ID: {id}");
                     return NotFound();
                 }
-
-                var response = _mapper.Map<LocationDTO>(item);
-                _logger.LogInfo($"{controllerName}: Success - Item ID: {id}");
+                var item = _npcRepo.FindByID(id);
+                var response = _mapper.Map<NpcDTO>(item);
+                _logger.LogInfo($"{controllerName}: Call Successful");
                 return Ok(response);
             }
             catch (Exception ex)
@@ -94,15 +93,15 @@ namespace player_log_api.Controllers
         }
 
         /// <summary>
-        /// Create a new Location record
+        /// Create a new Npc record
         /// </summary>
         /// <param name="itemDTO"></param>
-        /// <returns>Location record</returns>
+        /// <returns>Npc record</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreateLocation([FromBody] LocationDTO itemDTO)
+        public async Task<IActionResult> CreateNpc([FromBody] NpcDTO itemDTO)
         {
             var controllerName = GetControllerActionNames();
             try
@@ -110,7 +109,7 @@ namespace player_log_api.Controllers
                 _logger.LogInfo($"{controllerName}: Attempted Call");
                 if (itemDTO == null)
                 {
-                    _logger.LogWarn($"{controllerName}: Empty Request");
+                    _logger.LogWarn($"{controllerName}: Empty Request Body");
                     return BadRequest(ModelState);
                 }
                 if (!ModelState.IsValid)
@@ -118,9 +117,12 @@ namespace player_log_api.Controllers
                     _logger.LogWarn($"{controllerName}: Invalid Data");
                     return BadRequest(ModelState);
                 }
+
+                var item = _mapper.Map<Npc>(itemDTO);
+
                 _logger.LogInfo($"{controllerName}: Attempted Create");
-                var item = _mapper.Map<Location>(itemDTO);
-                var isSuccess = await _locRepo.Create(item);
+
+                var isSuccess = await _npcRepo.Create(item);
                 if (!isSuccess)
                 {
                     return InternalError($"{controllerName}: Create Failed");
@@ -135,7 +137,7 @@ namespace player_log_api.Controllers
         }
 
         /// <summary>
-        /// Update an existing Location record
+        /// Update an Npc record
         /// </summary>
         /// <param name="itemDTO"></param>
         /// <param name="id"></param>
@@ -145,34 +147,42 @@ namespace player_log_api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdateLocation([FromBody] LocationDTO itemDTO, int id)
+        public async Task<IActionResult> UpdateNpc([FromBody] NpcDTO itemDTO, int id)
         {
             var controllerName = GetControllerActionNames();
             try
             {
                 _logger.LogInfo($"{controllerName}: Attempted Call - Item ID: {id}");
-                if (id < 1 || itemDTO == null || id != itemDTO.LocationID)
+                if (id < 1 || id != itemDTO.NpcID)
                 {
                     _logger.LogWarn($"{controllerName}: Invalid ID - Item ID: {id}");
                     return BadRequest();
                 }
-                if (!await _locRepo.RecordExistsByID(id))
+                if (itemDTO == null)
                 {
-                    _logger.LogWarn($"{controllerName}: Not Found - Item ID: {id}");
-                    return NotFound();
+                    _logger.LogWarn($"{controllerName}: Empty Request Body");
+                    return BadRequest();
                 }
                 if (!ModelState.IsValid)
                 {
                     _logger.LogWarn($"{controllerName}: Invalid Data - Item ID: {id}");
                     return BadRequest(ModelState);
                 }
-                var item = _mapper.Map<Location>(itemDTO);
-                _logger.LogInfo($"{controllerName}: Attempted Update - Item ID: {id}");
-                var isSuccess = await _locRepo.Update(item);
+                if (!await _npcRepo.RecordExistsByID(id))
+                {
+                    _logger.LogWarn($"{controllerName}: Not Found - Item ID: {id}");
+                    return NotFound();
+                }
+
+                var item = _mapper.Map<Npc>(itemDTO);
+                _logger.LogInfo($"{controllerName}: Attempting Update - Item ID: {id}");
+                var isSuccess = await _npcRepo.Update(item);
+
                 if (!isSuccess)
                 {
                     return InternalError($"{controllerName}: Update Failed - Item ID: {id}");
                 }
+
                 _logger.LogInfo($"{controllerName}: Update Successful - Item ID: {id}");
                 return NoContent();
             }
@@ -183,7 +193,7 @@ namespace player_log_api.Controllers
         }
 
         /// <summary>
-        /// Delete a Location record
+        /// Delete an Npc record
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
@@ -192,36 +202,42 @@ namespace player_log_api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> DeleteLocation(int id)
+        public async Task<IActionResult> DeleteNpc(int id)
         {
             var controllerName = GetControllerActionNames();
             try
             {
-                _logger.LogInfo($"{controllerName}: Attempted Call - Item ID: {id}");
+                _logger.LogInfo($"{controllerName}: Attempting Call");
                 if (id < 1)
                 {
                     _logger.LogWarn($"{controllerName}: Invalid ID - Item ID: {id}");
                     return BadRequest();
                 }
-                if (!await _locRepo.RecordExistsByID(id))
+                if (!await _npcRepo.RecordExistsByID(id))
                 {
                     _logger.LogWarn($"{controllerName}: Not Found - Item ID: {id}");
                     return NotFound();
                 }
-                var item = await _locRepo.FindByID(id);
-                _logger.LogInfo($"{controllerName}: Attempted Delete - Item ID: {id}");
-                var isSuccess = await _locRepo.Delete(item);
+                var item = await _npcRepo.FindByID(id);
+                _logger.LogInfo($"{controllerName}: Attempting Delete - Item ID: {id}");
+                var isSuccess = await _npcRepo.Delete(item);
                 if (!isSuccess)
                 {
                     return InternalError($"{controllerName}: Delete Failed - Item ID: {id}");
                 }
-                _logger.LogInfo($"{controllerName}: Delete Successfull - Item ID: {id}");
+                _logger.LogInfo($"{controllerName}: Delete Successful - Item ID: {id}");
                 return NoContent();
             }
             catch (Exception ex)
             {
-                return InternalError($"{ex.Message} - {ex.InnerException}");
+                return InternalError($"{controllerName}: {ex.Message} - {ex.InnerException}");
             }
+        }
+
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong, contact administrator.");
         }
 
         private string GetControllerActionNames()
@@ -229,12 +245,6 @@ namespace player_log_api.Controllers
             var controller = ControllerContext.ActionDescriptor.ControllerName;
             var action = ControllerContext.ActionDescriptor.ActionName;
             return $"{controller} - {action}";
-        }
-
-        private ObjectResult InternalError(string message)
-        {
-            _logger.LogError(message);
-            return StatusCode(500, "Something went wrong, contact the administrator");
         }
     }
 }
