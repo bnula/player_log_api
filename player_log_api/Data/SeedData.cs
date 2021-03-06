52332a4840baf339fb77edb0324972fae2916786
bnula <bnulagnula@gmail.com>
2021-03-06 16:01:50 +0100

    added users controller with login and register endpoints

---
+++ player_log_api/Data/SeedData.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace player_log_api.Data
+{
+    public class SeedData
+    {
+        public async Task Seed(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            await SeedRoles(roleManager);
+            await SeedUsers(userManager);
+        }
+
+        private async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (await roleManager.RoleExistsAsync("administrator") == false)
+            {
+                var admin = new IdentityRole
+                { 
+                    Name = "administrator"
+                };
+
+                await roleManager.CreateAsync(admin);
+            }
+
+            if (await roleManager.RoleExistsAsync("user") == false)
+            {
+                var user = new IdentityRole
+                {
+                    Name = "user"
+                };
+
+                await roleManager.CreateAsync(user);
+            }
+        }
+
+        private async Task SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (await userManager.FindByEmailAsync("admin@page.com") == null)
+            {
+                var admin = new IdentityUser
+                {
+                    UserName = "admin@page.com",
+                    Email = "admin@page.com"
+                };
+
+                await userManager.CreateAsync(admin, "admin");
+            }
+
+            if (await userManager.FindByEmailAsync("user@page.com") == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "user1@page.com",
+                    Email = "user@page.com"
+                };
+
+                await userManager.CreateAsync(user, "123");
+
+            }
+        }
+    }
+}
--- player_log_api/Startup.cs
+++ player_log_api/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using player_log_api.Contracts;
 using player_log_api.Data;
@@ -17,6 +19,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace player_log_api
@@ -37,8 +40,32 @@ namespace player_log_api
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequiredLength = 1;
+                    options.Password.RequiredUniqueChars = 1;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
             
             services.AddControllers();
 
